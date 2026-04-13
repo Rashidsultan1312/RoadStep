81e76ef058a68dd99b224bc0a59b0868552a3fa6
Rashidsultan1312 <rashidsuk1io@gmail.com>
2026-04-13 15:23:35 +0300

    Update WebGateConfig.swift

--- CrossChick/WebGate/WebGateConfig.swift
+++ CrossChick/WebGate/WebGateConfig.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 struct WebGateConfig {
-    var apiURL: String = "https://mbr.baby/policy"
+    var apiURL: String = ""
     var timeout: TimeInterval = 10
     var fallback: WebGateResult = .facade
     var userAgent: String = "Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1"
